22af094d82dfe0f380695c4c9e76dc3465fa55bd
Nguyen Phuc Son <pson141002@gmail.com>
2022-07-25 01:20:02 +0700

    Just hide the nav and status bar, need more config for best exp

--- app/src/main/java/com/example/pson/smarttest/MainActivity.kt
+++ app/src/main/java/com/example/pson/smarttest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pson.smarttest
 
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,8 +11,27 @@ class MainActivity : AppCompatActivity() {
         //giấu status bar và actionBar
         supportActionBar?.hide()
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(R.layout.activity_main)
+
+        // TODO: Make the content under the nav and status bar so that UI stay the same
         //giấu thanh điều hướng
-        // TODO: code something here
+        window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            // Note that system bars will only be "visible" if none of the
+            // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN != 0) {
+                // The system bars are NOT visible. Make any desired
+                // adjustments to your UI, such as hiding the action bar or
+                // other navigational controls.
+                window.decorView.apply {
+                    // Hide both the navigation bar and the status bar.
+                    // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+                    // a general rule, you should design your app to hide the status bar whenever you
+                    // hide the navigation bar.
+                    systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+                }
+            }
+        }
+
+        setContentView(R.layout.activity_main)
     }
 }
