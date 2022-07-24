85c6a347d734d08ae33d041f7697cd8e88040276
Nguyen Phuc Son <pson141002@gmail.com>
2022-07-24 17:40:05 +0700

    Hi

--- app/src/main/java/com/example/pson/smarttest/MainActivity.kt
+++ app/src/main/java/com/example/pson/smarttest/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.pson.smarttest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //giấu status bar và actionBar
+        supportActionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        //giấu thanh điều hướng
+        // TODO: code something here
     }
 }
