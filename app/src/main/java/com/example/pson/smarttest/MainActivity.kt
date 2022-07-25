d4e096a28adc62f5a551613b1b80762e3822c877
Nguyen Phuc Son <pson141002@gmail.com>
2022-07-25 16:32:37 +0700

    Adjust so that layout is in immersive mode

--- app/src/main/java/com/example/pson/smarttest/MainActivity.kt
+++ app/src/main/java/com/example/pson/smarttest/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.pson.smarttest
 
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
-import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // TODO: Make the content under the nav and status bar so that UI stay the same
-
         setContentView(R.layout.activity_main)
+        //SET IMMERSIVE mode for layout
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     }
 }
